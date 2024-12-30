ce515440fd1965d428ded37d3b74d42033fa3fff
Sanchit9587 <sanchit9587@gmail.com>
2024-12-30 11:20:51 +0530

    Commented the merge sort algo

--- Sorting Algorithms/mergeSort.cpp
+++ Sorting Algorithms/mergeSort.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Divide and Merge
+
+//func that merges two arrays while sorting them
 void merge(vector<int> &vec,int low,int mid,int high)
 {
     vector<int> temp;
@@ -36,6 +39,7 @@ void merge(vector<int> &vec,int low,int mid,int high)
     }
 }
 
+//func that divides the arr, this is a recurssive function
 void mergeSort(vector<int> &vec,int low,int high)
 {
     if(low>=high) return;
@@ -45,8 +49,10 @@ void mergeSort(vector<int> &vec,int low,int high)
     merge(vec,low,mid,high);
 }
 
+
 int main()
 {
+    //taking input
     int n;
     cin>>n;
     vector<int> vec;
@@ -56,7 +62,10 @@ int main()
         cin>>temp;
         vec.push_back(temp);
     }
+
     mergeSort(vec,0,n-1);
+
+    //displaying the results
     for(int i=0;i<n;i++)
     {
         cout<<vec[i]<<" ";
